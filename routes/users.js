1551e89ad49666001be4bd5fe1346c69783f8473
Michał Woliński <s29239@pjwstk.edu.pl>
2024-12-28 19:18:57 +0100

    fix CORS

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const { registerNewUserHandler } = require('../controllers/UsersController');
-const cors = require('cors');
-
-router.use(cors());
 
 router.post('/', registerNewUserHandler);
 
